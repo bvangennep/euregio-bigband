279530c1b4010aec45aaba52e9909334030cca46
Bart van Gennep <b.vangennep@nerds.company>
2020-02-17 00:12:12 +0100

    Added graphql and polylang plugins

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,5 +1,8 @@
 <?php
 
+define('WP_PLUGIN_DIR', __DIR__.'/plugins');
+define('WP_PLUGIN_URL', getenv('HOME_URL').'/plugins');
+
 require_once __DIR__.'/../config/db.php';
 require_once __DIR__.'/../config/secrets.php';
 
